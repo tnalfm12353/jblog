543a72ce238d6c23709c98b50ebd1c863be701a1
hong <tnalfm12353@naver.com>
2021-06-16 11:55:22 +0900

    jblog

---
+++ jblog03/src/main/java/com/douzone/jblog/controller/BlogController.java
@@ -0,0 +1,10 @@
+package com.douzone.jblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/{id}")
+public class BlogController {
+
+}
